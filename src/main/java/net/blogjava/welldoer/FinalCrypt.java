9a8f72f90b972bc362b1c920a0804feb44f0ca97
welldoer <welldoer@263.net>
2019-03-06 21:05:38 +0800

    :white_check_mark: 新增加密功能及测试

--- src/main/java/net/blogjava/welldoer/FinalCrypt.java
+++ src/main/java/net/blogjava/welldoer/FinalCrypt.java
@@ -1,5 +1,27 @@
 package net.blogjava.welldoer;
 
+import java.nio.ByteBuffer;
+
 public class FinalCrypt {
+
+	public byte encryptByte(byte data, byte cipher ) {
+		int dim = 0;		// data unfiltered mask
+		int dum = 0;		// data filtered mask
+		int duim = 0;		// data unfiltered & filtered mask
+		
+		dum = data & ~cipher;
+		dim = ~data & cipher;
+		duim = dum + dim;
+		
+		return ( byte ) duim;
+	}
+
+	public ByteBuffer cryptOutputBuffer(ByteBuffer dataBuffer, ByteBuffer cipherBuffer) {
+		ByteBuffer outputBuffer = ByteBuffer.allocate( dataBuffer.capacity() );
+		for( byte dataByte : dataBuffer.array() ) {
+			outputBuffer.put( encryptByte( dataByte, cipherBuffer.get() ) );
+		}
+		return outputBuffer;
+	}
 
 }
--- src/test/java/net/blogjava/welldoer/FinalCryptTest.java
+++ src/test/java/net/blogjava/welldoer/FinalCryptTest.java
@@ -3,6 +3,9 @@ package net.blogjava.welldoer;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,8 +25,128 @@ public class FinalCryptTest {
 	}
 
 	@Test
-	public void test() {
-		assertThat( true ).isEqualTo( true );
+	public void testEncryptByteWithSameParameters() {
+		assertThat( String.format( "%8s", Integer.toBinaryString( 0 & 0xFF ) ).replace( ' ', '0' ) ).isEqualTo( "00000000" );
+		assertThat( String.format( "%8s", Integer.toBinaryString( 257 & 0xFF ) ).replace( ' ', '0' ) ).isEqualTo( "00000001" );
+		assertThat( finalCrypt.encryptByte( ( byte ) 0, ( byte ) 0 ) ).isEqualTo( ( byte ) 0 );
+	}
+
+	@Test
+	public void testEncryptByteWithTypicalParameters() {
+		assertThat( String.format( "%8s", Integer.toBinaryString( 5 & 0xFF ) ).replace( ' ', '0' ) ).isEqualTo( "00000101" );
+		assertThat( finalCrypt.encryptByte( ( byte ) 5, ( byte ) 3 ) ).isEqualTo( ( byte ) 6 );
+		assertThat( finalCrypt.encryptByte( ( byte ) 6, ( byte ) 3 ) ).isEqualTo( ( byte ) 5 );
+	}
+	
+	@Test
+	public void testCryptOutputBuffer() {
+		ByteBuffer dataBuffer = ByteBuffer.wrap( new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		ByteBuffer cipherBuffer = ByteBuffer.allocate( 13 );
+		cipherBuffer.put( new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } );
+		ByteBuffer outputBuffer = ByteBuffer.allocate( cipherBuffer.capacity() );
+		outputBuffer.put( new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } );
+		dataBuffer.flip();
+		cipherBuffer.flip();
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 0 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( cipherBuffer.position() ).isEqualTo( 0 );
+		assertThat( cipherBuffer.limit() ).isEqualTo( 13 );
+		assertThat( cipherBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( finalCrypt.cryptOutputBuffer( dataBuffer, cipherBuffer ) ).isEqualToComparingFieldByField( outputBuffer );
+	}
+
+	@Test
+	public void testByteBuffer() {
+		ByteBuffer dataBuffer = ByteBuffer.wrap( new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( dataBuffer.get( 1 ) ).isEqualTo( ( byte ) 1 );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 0 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 1 );
+		assertThat( dataBuffer.position() ).isEqualTo( 2 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.flip();
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 2 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 0 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 1 );
+		assertThat( dataBuffer.position() ).isEqualTo( 2 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 2 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.flip();
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 2 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 0 );
+		assertThat( dataBuffer.position() ).isEqualTo( 1 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 2 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+	}
+
+	@Test( expected = BufferUnderflowException.class )
+	public void testByteBufferWrapAndFlip() {
+		ByteBuffer dataBuffer = ByteBuffer.wrap( new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.flip();
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 0 );
+	}
+	
+	@Test
+	public void testNewByteBuffer() {
+		ByteBuffer dataBuffer = ByteBuffer.allocate( 13 );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.put( new byte[] { 0, 1, 2 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 3 );
+		dataBuffer.put( new byte[] { 3, 4, 5, 6 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 7 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		dataBuffer.put( new byte[] { 7, 8, 9, 10 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 11 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.flip();
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 0 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 1 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 2 );
+		assertThat( dataBuffer.position() ).isEqualTo( 3 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 11 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.flip();
+		dataBuffer.put( new byte[] { 100, 101 } );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 2 );
+		assertThat( dataBuffer.position() ).isEqualTo( 3 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 3 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+	}
+
+	@Test
+	public void testNewByteBufferWrap() {
+		ByteBuffer dataBuffer = ByteBuffer.wrap( new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 0 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 13 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
+		dataBuffer.put( new byte[] { 100, 101, 102, 103, 104 } );
+		assertThat( dataBuffer.position() ).isEqualTo( 5 );
+		dataBuffer.flip();
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 100 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 101 );
+		assertThat( dataBuffer.get() ).isEqualTo( ( byte ) 102 );
+		assertThat( dataBuffer.position() ).isEqualTo( 3 );
+		assertThat( dataBuffer.limit() ).isEqualTo( 5 );
+		assertThat( dataBuffer.capacity() ).isEqualTo( 13 );
 	}
 
 }
